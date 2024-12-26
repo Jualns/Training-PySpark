d4e2e5594c6addcdc940a9904e2e8e5c6b728cc9
João Victor da Silva <josilva602@gmail.com>
2024-12-26 14:45:06 +0000

    Add new solution using pyspark without SQL

--- solutions/sparkplayground/Load and Transform Data.py
+++ solutions/sparkplayground/Load and Transform Data.py
@@ -1,11 +1,13 @@
 # Initialize Spark session
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
 spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
 
 try:
 
     #enter the file path here
-    file_path = "/workspaces/Training-PySpark/datasets/customers.csv"
+    file_path = "../Training-PySpark/datasets/customers.csv"
     
 
     #read the file
@@ -25,6 +27,13 @@ try:
 
     # Display the final DataFrame using the display() function.
     selected_df.show()
+
+    # Solution based on pyspark without SQL
+    selected_df_py = df.filter((df.purchase_amount >= 100) & (df.age >= 30)) \
+                .select(col("customer_id"), col("name"), col("purchase_amount"))
+
+    selected_df_py.show()
+
 except Exception as err:
     print(f"Erro: {err}")
 finally:
